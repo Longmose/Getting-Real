2ff0a49268136b855c385823d8d5b26a72230951
daniel lundgaard! <daniellundgaard@live.dk>
2024-05-14 11:20:20 +0200

    bestillings commit

---
+++ Bestilling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullyShipd
+{
+    public class Bestilling
+    {
+
+        public int Id { get; set; }                 // Bestillings-id
+        public List<string> OrdreIdListe { get; set; } // Liste over ordre-ids
+        public TimeSpan ForventetLeveringstid { get; set; } // Forventet leveringstid
+
+
+        public Bestilling(int id, List<string> ordreIdListe, TimeSpan forventetLeveringstid)
+        {
+            Id = id;
+            OrdreIdListe = ordreIdListe;
+            ForventetLeveringstid = forventetLeveringstid;
+        }
+
+        // Metode til at tilføje en ordre-id til listen
+        public void TilføjOrdreId(string ordreId)
+        {
+            OrdreIdListe.Add(ordreId);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Ordre ordre = new Ordre(); 
-            
+            Ordre ordre = new Ordre();
+
+            // Opret en ny bestilling
+            Bestilling bestilling1 = new Bestilling(1, new List<string>(), TimeSpan.FromDays(3));
+
+            // Tilføj en ordre-id til bestillingen
+            bestilling1.TilføjOrdreId("");
+
+            // Udskriv bestillingsoplysninger
+            Console.WriteLine("Bestillings-ID: " + bestilling1.Id);
+            Console.WriteLine("Ordre-IDs: ");
+            foreach (string ordreId in bestilling1.OrdreIdListe)
+            {
+                Console.WriteLine(ordreId);
+            }
+            Console.WriteLine("Forventet leveringstid: " + bestilling1.ForventetLeveringstid.TotalDays + " dage");
         }
     }
 }
